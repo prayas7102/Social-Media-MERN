e24f452024d7b87d737212f0c40ce30ac8ce9c41
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-03-14 06:35:14 +0530

    error removed

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: [true, "please enter value"], },
-    email: { type: String, required: [true, "please enter value"], unique: [true, "email already exists"] },
-    password: { type: String, required: [true, "please enter password"], minilength: [6, "password must be 6 characters"], select: false, },
+    name: { 
+        type: String, 
+        required: [true, "please enter value"], 
+    },
+    email: { 
+        type: String, 
+        required: [true, "please enter value"], 
+        unique: [true, "email already exists"] 
+    },
+    password: { 
+        type: String, 
+        required: [true, "please enter password"], 
+        minilength: [6, "password must be 6 characters"], 
+        select: false, 
+    },
     posts: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Post",
--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,4 @@
 const app = require("./app");
-app.listen(process.env.PORT, () => { console.log(`Server is running on ${process.env.PORT}`) })
+app.listen(process.env.PORT, () => { 
+    console.log(`Server is running on ${process.env.PORT}`) 
+});
