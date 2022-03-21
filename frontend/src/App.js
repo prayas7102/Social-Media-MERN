df5c16be30b26f90be610616940948136202f45d
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-03-21 15:56:03 +0530

    frontend setup done

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import {BrowserRouter as Router} from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
