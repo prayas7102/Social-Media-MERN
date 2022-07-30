cda4adc099c8efd77ba8b2aac1fc811238bfbc1b
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-07-30 09:47:11 +0530

    All css added

---
+++ frontend/src/components/CommonCard/CommonCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CommonCard() {
+  return (
+    <div>CommonCard</div>
+  )
+}
+
+export default CommonCard
---
+++ frontend/src/components/UpdateProfile/UpdatProfile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UpdatProfile() {
+  return (
+    <div>UpdatProfile</div>
+  )
+}
+
+export default UpdatProfile
